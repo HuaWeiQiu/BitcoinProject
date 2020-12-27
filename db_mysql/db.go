5bf2863d1aa9987ccd93409ac0906c20530b8278
Bard69 <k2942318217@163.com>
2020-12-27 13:57:38 +0800

    xcp

---
+++ db_mysql/db.go
@@ -0,0 +1,26 @@
+package db_mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/astaxie/beego"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Db *sql.DB
+
+func ConnectDB()  {
+	config := beego.AppConfig
+	dbDriver := config.String("driverName")
+	dbUser := config.String("db_user")
+	dbPassword := config.String("db_password")
+	dbIp := config.String("db_ip")
+	dbName := config.String("db_name")
+	connUrl := dbUser + ":" + dbPassword + "@tcp(" + dbIp + ")/" + dbName + "?charset=utf8"
+	db, err := sql.Open(dbDriver, connUrl)
+	if err != nil {
+	panic("数据库连接错误!!!")
+	}
+	fmt.Println("数据库连接")
+	Db = db
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"BitcoinProject/db_mysql"
 	_ "BitcoinProject/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	db_mysql.ConnectDB()
 	beego.Run()
 }
 
