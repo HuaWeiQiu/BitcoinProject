c6579387c63a9c7e92e7d82f4ef8c62213e89fcc
qhw1987752122 <qhw1987752122@163.com>
2020-12-27 20:59:13 +0800

    ye

--- main.go
+++ main.go
@@ -7,14 +7,10 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 	db_mysql.ConnectDB()
-=======
 	//设置静态资源路径
 	beego.SetStaticPath("/js","./static/js")
 	beego.SetStaticPath("/css","./static/css")
 	beego.SetStaticPath("/img","./static/img")
-
->>>>>>> remotes/origin/ye
 	beego.Run()
 }
